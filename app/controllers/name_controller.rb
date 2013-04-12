683cc75e37dbdd2a62019b91877fc24f59fef0c8
Stephen Philp <stephen@stephenphilp.com>
2013-04-12 12:29:29 -0700

    http works, but blows away @data! why?

--- app/controllers/name_controller.rb
+++ app/controllers/name_controller.rb
@@ -35,44 +35,15 @@ class NameController < UIViewController
 
   def get_names(gender)
     raise ArgumentError, "invalid gender type #{gender}" unless ['male','female'].include? gender
-    # TODO: This will need to:
-    # call the server to get a list of names as json, 
-    # instantiate them
-    # and return as an array of Name objects
+    # TODO: Why is @data empty after this call?
 
-    # For testing just create the array of name objects
+    BW::HTTP.get("http://localhost:3000/names.json/") do |response|
+      array_of_hashes = BW::JSON.parse(response.body.to_str)
+      array_of_hashes.each { |h| @data << Name.new(h) }
+    end 
 
-    @data =[ Name.new({id: 1, spelling: 'A. J.', phonetic: 'AY - Jay'}) ,
-Name.new({id: 2, spelling: 'Aaliyah', phonetic: 'Ah - LEE - Ah'}) ,
-Name.new({id: 3, spelling: 'Aari', phonetic: 'ARE - Ee'}) ,
-Name.new({id: 4, spelling: 'Aariella', phonetic: 'Are - Ee - ELL - Ah'}) ,
-Name.new({id: 5, spelling: 'Aaron', phonetic: 'AIR - Ren'}) ,
-Name.new({id: 6, spelling: 'Aaron', phonetic: 'AR - En'}) ,
-Name.new({id: 7, spelling: 'Abbey', phonetic: 'ABB - Bee'}) ,
-Name.new({id: 8, spelling: 'Abbigail', phonetic: 'ABB - Eh - Gail'}) ,
-Name.new({id: 9, spelling: 'Abdiel', phonetic: 'AB - Dee -Ell'}) ,
-Name.new({id: 10, spelling: 'Abdullah', phonetic: 'Abb - DOO - Lah'}) ,
-Name.new({id: 11, spelling: 'Abel', phonetic: 'AYB - Ell'}) ,
-Name.new({id: 12, spelling: 'Abraham', phonetic: 'AYB - Brah - Ham'}) ,
-Name.new({id: 13, spelling: 'Abram', phonetic: 'AY - Bram'}) ,
-Name.new({id: 14, spelling: 'Abrielle', phonetic: 'AY - Bree - El'}) ,
-Name.new({id: 15, spelling: 'Acacia', phonetic: 'Ah - KAY - Shah'}) ,
-Name.new({id: 16, spelling: 'Adair', phonetic: 'Ah - DAIR'}) ,
-Name.new({id: 17, spelling: 'Adam', phonetic: 'ADD - Amm'}) ,
-Name.new({id: 18, spelling: 'Addie', phonetic: 'ADD - Ee' }) ,
-Name.new({id: 19, spelling: 'Addison', phonetic: 'ADD - Eh - Son' }) ,
-Name.new({id: 20, spelling: 'Adelaide', phonetic: 'AD - Ah - Laid' }) ,
-Name.new({id: 21, spelling: 'Adeline', phonetic: 'ADD - Eh - Lyen' }) ,
-Name.new({id: 22, spelling: 'Adelle', phonetic: 'Ah - DELL' }) ,
-Name.new({id: 23, spelling: 'Adolfo', phonetic: 'Ah - DOLF - Oh' }) ,
-Name.new({id: 24, spelling: 'Adonis', phonetic: 'Ah - DON - Iss' }) ,
-Name.new({id: 25, spelling: 'Adrian', phonetic: 'AYD - Dree - An'}) ,
-Name.new({id: 26, spelling: 'Adriana', phonetic: 'Ayd - Dree - AUN - Ah' }) ,
-Name.new({id: 27, spelling: 'Adrianna', phonetic: 'AY - Dree - ANN - Ah'}) ,
-Name.new({id: 28, spelling: 'Adriel', phonetic: 'ADD - Ree - Ell'}) ,
-Name.new({id: 29, spelling: 'Agnes', phonetic: 'AG - Nes' }) ,
-Name.new({id: 30, spelling: 'Ahmad', phonetic: 'Ah - MAHD' }) ,
-Name.new({id: 31, spelling: 'Ahmed', phonetic: 'Ah - MED'}) ]
-
+    @data
   end
+
+
 end
--- spec/name_controller_spec.rb
+++ spec/name_controller_spec.rb
@@ -5,9 +5,16 @@ describe "NameController" do
     it 'should throw an error if bad gender' do 
       should.raise(ArgumentError) { controller.get_names('alien') }
     end
-    it "should populate data" do 
-      controller.get_names('female')
-      controller.instance_variable_get("@data").count.should == 31
+  #  it "should populate data" do 
+  #    controller.get_names('female')
+  #    controller.instance_variable_get("@data").count.should == 31
+  #  end
+  end
+
+  describe '#parse_name' do 
+    it 'should set the attributes' do 
+      controller.parse_name()
     end
   end
+
 end
